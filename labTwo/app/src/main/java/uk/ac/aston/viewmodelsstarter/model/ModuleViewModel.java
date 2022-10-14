d64b3a4977e6770152a540fe3081f6e96a810183
Nathan <nathan_byrne96@hotmail.co.uk>
2022-10-14 14:13:17 +0100

    add lab 2 & 3

---
+++ labTwo/app/src/main/java/uk/ac/aston/viewmodelsstarter/model/ModuleViewModel.java
@@ -0,0 +1,31 @@
+package uk.ac.aston.viewmodelsstarter.model;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import uk.ac.aston.viewmodelsstarter.MyApplication;
+
+public class ModuleViewModel extends ViewModel {
+    private MutableLiveData<Module> currentModule;
+
+    public ModuleViewModel() {
+        super();
+        currentModule = new MutableLiveData<Module>();
+        setModuleData(); // only for dummy data
+    }
+
+    public LiveData<Module> getCurrentModule() {
+        return currentModule;
+    }
+
+    private void setModuleData() {
+        currentModule.setValue(DataSource.getInstance(MyApplication.getAppContext()).getRealModule());
+    }
+
+    public void setCurrentModuleTitle(String newTitle) {
+        Module m = currentModule.getValue();
+        m.setTitle(newTitle);
+        currentModule.setValue(m);
+    }
+}
