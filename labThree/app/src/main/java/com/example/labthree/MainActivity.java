d70c5d02cd372728e381ba4046cff337e100ae34
Nathan <nathan_byrne96@hotmail.co.uk>
2022-10-17 19:10:42 +0100

    add lab three

--- labThree/app/src/main/java/com/example/labthree/MainActivity.java
+++ labThree/app/src/main/java/com/example/labthree/MainActivity.java
@@ -1,14 +1,256 @@
 package com.example.labthree;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.google.android.gms.location.LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "NB TAG";
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location myCurrentLocation;
+    private boolean requestingLocationUpdates;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private TextView latitudeText;
+    private TextView longitudeText;
+    private TextView timestampText;
+    private TextView addressText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        myCurrentLocation = null;
+        requestingLocationUpdates = false;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Location permissions have not been granted");
+        } else {
+            Log.i(TAG, "Location permissions already granted.");
+            getLastLocation();
+        }
+
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts
+                                .RequestMultiplePermissions(), result -> {
+                    Boolean fineLocationGranted = null;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                        fineLocationGranted = result.getOrDefault(
+                                Manifest.permission.ACCESS_FINE_LOCATION, false);
+                    } else {
+                        fineLocationGranted =
+                                result.get(Manifest.permission.ACCESS_FINE_LOCATION) != null ?
+                                        result.get(Manifest.permission.ACCESS_FINE_LOCATION) :
+                                        false;
+                    }
+                    Boolean coarseLocationGranted = null;
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                        coarseLocationGranted = result.getOrDefault(
+                                Manifest.permission.ACCESS_COARSE_LOCATION, false);
+                    } else {
+                        coarseLocationGranted =
+                                result.get(Manifest.permission.ACCESS_COARSE_LOCATION) != null ?
+                                        result.get(Manifest.permission.ACCESS_COARSE_LOCATION) :
+                                        false;
+                    }
+
+                    if (fineLocationGranted != null && fineLocationGranted) {
+                        Log.i(TAG, "Precise location access granted.");
+                        getLastLocation();
+                    } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                        Log.i(TAG, "Only approximate location access granted.");
+                        getLastLocation();
+                    } else {
+                        Log.i(TAG, "No location access granted.");
+                    }
+                        }
+                );
+
+        // Before you perform the actual permission request, check whether your app
+        // already has the permissions, and whether your app needs to show a permission
+        // rationale dialog. For more details, see Request permissions.
+        locationPermissionRequest.launch(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    Log.i(TAG, "Location Update: NO LOCATION");
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    // Update UI with location data
+                    Log.i(TAG, "Location Update: (" + location.getLatitude() +
+                            ", " + location.getLongitude() + ")");
+//                    myCurrentLocation = location;
+                    updateUILocation(location);
+                }
+            }
+        };
+
+        Button updatesButton = findViewById(R.id.updates_button);
+        updatesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (requestingLocationUpdates == false) {
+                    // starting
+                    requestingLocationUpdates = true;
+                    updatesButton.setText("Stop Location Updates");
+                    startLocationUpdates();
+                    Log.i(TAG, "Updates started");
+                } else {
+                    // stopping
+                    requestingLocationUpdates = false;
+                    updatesButton.setText("Start Location Updates");
+                    stopLocationUpdates();
+                    Log.i(TAG, "Updates stopped");
+                }
+            }
+        });
+
+        latitudeText = findViewById(R.id.latitude);
+        longitudeText = findViewById(R.id.longitude);
+        timestampText = findViewById(R.id.timestamp);
+        addressText = findViewById(R.id.address);
+    }
+
+    private void updateUILocation(Location location) {
+        this.myCurrentLocation = location;
+        latitudeText.setText("Lat: " + location.getLatitude());
+        longitudeText.setText("Lon: " + location.getLongitude());
+
+        Date date = new Date(location.getTime());
+        DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+        //formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+        String dateFormatted = formatter.format(date);
+        timestampText.setText("Last update: " + dateFormatted);
+
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+            String postalCode = addresses.get(0).getPostalCode();
+            String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
+            String addressFormatted = address + "\n" + city + "\n" + state + "\n" +
+                    country + "\n" + postalCode + "\n" + knownName;
+            this.addressText.setText(addressFormatted);
+        } catch (IOException e) {
+            Log.e(TAG, "Error getting address\n" + e.getMessage());
+        }
+    }
+
+
+    public void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            //    here to request the missing permissions, and then overriding
+            //    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            //    to handle the case where the user grants the permission. See the documentation
+            //    for ActivityCompat#requestPermissions for more details.
+
+            String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+            ActivityCompat.requestPermissions(this, permissions, 1);
+            return;
+        }
+
+        fusedLocationClient.getCurrentLocation(PRIORITY_BALANCED_POWER_ACCURACY, null)
+                //fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+//                            myCurrentLocation = location;
+                            updateUILocation(location);
+                            // Logic to handle location object
+                            Log.i(TAG, "We got a location: (" + location.getLatitude() +
+                                    ", " + location.getLongitude() + ")");
+                        } else {
+                            Log.i(TAG, "We failed to get a last location");
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (requestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    private void startLocationUpdates() {
+        createLocationRequest();
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+                locationCallback,
+                Looper.getMainLooper());
+    }
+
+    protected void createLocationRequest() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+    private void stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback);
     }
 }
